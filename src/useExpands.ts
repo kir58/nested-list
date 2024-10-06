0c77658bd3ec4aa06afb1adbe12bc77d53bf7809
kir <advtrail@yandex.ru>
2024-10-06 22:59:18 +0300

    fixed

--- src/useExpands.ts
+++ src/useExpands.ts
@@ -3,13 +3,13 @@ import { useState } from 'react';
 
 type Props = { data: Item };
 
-const getAllParentsId = (data: Item[]): number[] =>
-  data.reduce<number[]>((acc, item) => {
+const getAllParentsId = (data: Item): number[] =>
+  data.children.reduce<number[]>((acc, item) => {
     if (!item.children.length) {
       return acc;
     }
-    return [...acc, item.id, ...getAllParentsId(item.children)];
-  }, []);
+    return [...acc, item.id, ...getAllParentsId(item)];
+  }, [data.id]);
 
 export const useExpands = ({ data }: Props) => {
   const [expandedIds, setExpandedIds] = useState<number[]>([]);
@@ -18,17 +18,18 @@ export const useExpands = ({ data }: Props) => {
     if (expandedIds.length) {
       setExpandedIds([]);
     } else {
-      setExpandedIds(getAllParentsId(data.children));
+      setExpandedIds(getAllParentsId(data));
     }
   };
 
-  const isEnabled = data.children.some(({ children }) => children.length);
+
+  const isDisabled = !data.children.length
 
   return {
     onExpandAll,
     expandedIds,
     setExpandedIds,
     isExpanded: !!expandedIds.length,
-    isDisabled: !isEnabled,
+    isDisabled,
   };
 };
